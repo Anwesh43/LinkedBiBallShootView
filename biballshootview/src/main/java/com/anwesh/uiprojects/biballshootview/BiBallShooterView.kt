f6095b17c308190a0aea401e1fbfab19069011b0
Anwesh43 <anweshthecool0@gmail.com>
2020-09-03 17:25:02 +0530

    adding BiBallShooterView class

--- biballshootview/src/main/java/com/anwesh/uiprojects/biballshootview/BiBallShooterView.kt
+++ biballshootview/src/main/java/com/anwesh/uiprojects/biballshootview/BiBallShooterView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawBBSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiBallShooter(scale, w, h, paint)
 }
+
+class BiBallShooterView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
