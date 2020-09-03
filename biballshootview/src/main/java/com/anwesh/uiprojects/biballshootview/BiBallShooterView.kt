0e8803d5f1a3d249e586594cc3e454f223ea5966
Anwesh43 <anweshthecool0@gmail.com>
2020-09-03 18:31:47 +0530

    adding BBSNode class

--- biballshootview/src/main/java/com/anwesh/uiprojects/biballshootview/BiBallShooterView.kt
+++ biballshootview/src/main/java/com/anwesh/uiprojects/biballshootview/BiBallShooterView.kt
@@ -134,4 +134,45 @@ class BiBallShooterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBSNode(var i : Int, val state : State = State()) {
+
+        private var next : BBSNode? = null
+        private var prev : BBSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BBSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBSNode {
+            var curr : BBSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
